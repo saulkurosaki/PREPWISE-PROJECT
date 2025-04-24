054ec687669103813dc9cc1670b1c2e540ee7ec3
saulkurosaki <saulkurosaki13@gmail.com>
2025-04-24 16:59:02 -0600

    feat(FormField): implement the field type to the form field

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -30,7 +30,12 @@ const FormField = ({
       <FormItem>
         <FormLabel className="label">{label}</FormLabel>
         <FormControl>
-          <Input className="input" placeholder={placeholder} {...field} />
+          <Input
+            className="input"
+            placeholder={placeholder}
+            type={type}
+            {...field}
+          />
         </FormControl>
 
         <FormMessage />
