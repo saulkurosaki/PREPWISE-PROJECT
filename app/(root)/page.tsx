52be4e2f0d7828d3c3c54b9a73e72005149b34f4
saulkurosaki <saulkurosaki13@gmail.com>
2025-04-25 13:22:04 -0600

    feat(HomePage): implement the take an interview section initial structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -26,12 +26,21 @@ const HomePage = () => {
         />
       </section>
 
-      {/* INTERVIEWS SECTION */}
+      {/* YOUR INTERVIEWS SECTION */}
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
 
         <div className="interviews-section">
           <p>You haven&apos;t taken any interviews yet</p>
+        </div>
+      </section>
+
+      {/* TAKE AN INTERVIEW SECTION */}
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+
+        <div className="interviews-section">
+          <p>There are no interviews available</p>
         </div>
       </section>
     </>
