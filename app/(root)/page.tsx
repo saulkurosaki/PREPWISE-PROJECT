bcfbb9299c7bca9c8d14f9fa92c5bc6d1341d03b
saulkurosaki <saulkurosaki13@gmail.com>
2025-04-25 13:11:27 -0600

    feat(HomePage): implement the card cta hero section title and subtitle

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,17 @@
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      {/* CARD CTA */}
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice on real interview questions & get instant feedback
+          </p>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default HomePage;
