28fa828b428e726376e62cce3316b74382a381b2
saulkurosaki <saulkurosaki13@gmail.com>
2025-04-25 13:02:47 -0600

    feat(Layout): rename the RootLayout to Layout in order to keep name divisions between the layout files

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   description: "An AI-powered platform for preparing for mock interviews.",
 };
 
-export default function RootLayout({
+export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
