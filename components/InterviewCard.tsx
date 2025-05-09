b3ea4b1eaa9e0087af9284b722b3a86c7661f357
saulkurosaki <saulkurosaki13@gmail.com>
2025-05-09 13:47:16 -0600

    feat(InterviewCard): implement the dayjs library day format for the interview card component

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,6 +1,9 @@
+import dayjs from 'dayjs';
+
 const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
   const feedback = null as Feedback | null;
   const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
+  const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY');
 
   return <div>InterviewCard</div>;
 };
