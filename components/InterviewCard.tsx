53e96eb574d79d3905c766e90368dc404e1f4725
saulkurosaki <saulkurosaki13@gmail.com>
2025-05-09 13:51:08 -0600

    feat(InterviewCard): implement the interview card initial structure along the normalized type of the card badge at the top right

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,11 +1,30 @@
-import dayjs from 'dayjs';
+import dayjs from "dayjs";
 
-const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
+const InterviewCard = ({
+  interviewId,
+  userId,
+  role,
+  type,
+  techstack,
+  createdAt,
+}: InterviewCardProps) => {
   const feedback = null as Feedback | null;
-  const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
-  const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY');
+  const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
+  const formattedDate = dayjs(
+    feedback?.createdAt || createdAt || Date.now()
+  ).format("MMM D, YYYY");
 
-  return <div>InterviewCard</div>;
+  return (
+    <div className="card-border w-[360px] max-sm:w-full min-h-96">
+      <div className="card-interview">
+        <div>
+          <div className="absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600">
+            <p className="badge-text">{normalizedType}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default InterviewCard;
