81e797f687caef695e06fecb7fb6d4fdf76bcaad
saulkurosaki <saulkurosaki13@gmail.com>
2025-05-30 23:09:40 -0600

    feat(auth.actions.ts): implement the complete getCurrentUser server action function

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -97,33 +97,33 @@ export async function signOut() {
 }
 
 // Get current user from session cookie
-// export async function getCurrentUser(): Promise<User | null> {
-//   const cookieStore = await cookies();
+export async function getCurrentUser(): Promise<User | null> {
+  const cookieStore = await cookies();
 
-//   const sessionCookie = cookieStore.get("session")?.value;
-//   if (!sessionCookie) return null;
+  const sessionCookie = cookieStore.get("session")?.value;
+  if (!sessionCookie) return null;
 
-//   try {
-//     const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
 
-//     // get user info from db
-//     const userRecord = await db
-//       .collection("users")
-//       .doc(decodedClaims.uid)
-//       .get();
-//     if (!userRecord.exists) return null;
+    // get user info from db
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+    if (!userRecord.exists) return null;
 
-//     return {
-//       ...userRecord.data(),
-//       id: userRecord.id,
-//     } as User;
-//   } catch (error) {
-//     console.log(error);
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (error) {
+    console.log(error);
 
-//     // Invalid or expired session
-//     return null;
-//   }
-// }
+    // Invalid or expired session
+    return null;
+  }
+}
 
 // Check if user is authenticated
 // export async function isAuthenticated() {
