65e526e1edfdd887da2a40aca56622509e622a0c
saulkurosaki <saulkurosaki13@gmail.com>
2025-05-30 23:08:48 -0600

    feat(auth.actions.ts): implement the complete signUp server action function

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -25,47 +25,47 @@ export async function setSessionCookie(idToken: string) {
   });
 }
 
-// export async function signUp(params: SignUpParams) {
-//   const { uid, name, email } = params;
+export async function signUp(params: SignUpParams) {
+  const { uid, name, email } = params;
 
-//   try {
-//     // check if user exists in db
-//     const userRecord = await db.collection("users").doc(uid).get();
-//     if (userRecord.exists)
-//       return {
-//         success: false,
-//         message: "User already exists. Please sign in.",
-//       };
+  try {
+    // check if user exists in db
+    const userRecord = await db.collection("users").doc(uid).get();
+    if (userRecord.exists)
+      return {
+        success: false,
+        message: "User already exists. Please sign in.",
+      };
 
-//     // save user to db
-//     await db.collection("users").doc(uid).set({
-//       name,
-//       email,
-//       // profileURL,
-//       // resumeURL,
-//     });
+    // save user to db
+    await db.collection("users").doc(uid).set({
+      name,
+      email,
+      // profileURL,
+      // resumeURL,
+    });
 
-//     return {
-//       success: true,
-//       message: "Account created successfully. Please sign in.",
-//     };
-//   } catch (error: any) {
-//     console.error("Error creating user:", error);
+    return {
+      success: true,
+      message: "Account created successfully. Please sign in.",
+    };
+  } catch (error: any) {
+    console.error("Error creating user:", error);
 
-//     // Handle Firebase specific errors
-//     if (error.code === "auth/email-already-exists") {
-//       return {
-//         success: false,
-//         message: "This email is already in use",
-//       };
-//     }
+    // Handle Firebase specific errors
+    if (error.code === "auth/email-already-exists") {
+      return {
+        success: false,
+        message: "This email is already in use",
+      };
+    }
 
-//     return {
-//       success: false,
-//       message: "Failed to create account. Please try again.",
-//     };
-//   }
-// }
+    return {
+      success: false,
+      message: "Failed to create account. Please try again.",
+    };
+  }
+}
 
 // export async function signIn(params: SignInParams) {
 //   const { email, idToken } = params;
