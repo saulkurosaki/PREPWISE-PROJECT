9f09eebc01765efbd9e3fb651271378182cffb4b
saulkurosaki <saulkurosaki13@gmail.com>
2025-07-16 18:37:31 -0600

    feat(Agent.tsx): implement the vapi avatar within the interviewer card section

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,5 +1,21 @@
+import Image from "next/image";
+
 const Agent = () => {
-  return <div>Agent</div>;
+  return (
+    <div className="call-view">
+      <div className="card-interviewer">
+        <div className="avatar">
+          <Image
+            src="/ai-avatar.png"
+            alt="vapi"
+            width={65}
+            height={54}
+            className="object-cover"
+          ></Image>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Agent;
