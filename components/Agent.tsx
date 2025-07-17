e5ff65d79f81a3844db6470fd7a75407235dbfd0
saulkurosaki <saulkurosaki13@gmail.com>
2025-07-16 18:44:50 -0600

    feat(Agent.tsx): implement the interviewer card final part as well as the interviewee user card

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 
-const Agent = () => {
+const Agent = ({ userName, userId, type }: AgentProps) => {
+  const isSpeaking = true;
+
   return (
     <div className="call-view">
       <div className="card-interviewer">
@@ -11,7 +13,22 @@ const Agent = () => {
             width={65}
             height={54}
             className="object-cover"
-          ></Image>
+          />
+          {isSpeaking && <span className="animate-speak"></span>}
+        </div>
+        <h3>AI Interviewer</h3>
+      </div>
+
+      <div className="card-border">
+        <div className="card-content">
+          <Image
+            src="/ai-avatar.png"
+            alt="user avatar"
+            width={540}
+            height={540}
+            className="object-cover rounded-full size-[120px]"
+          />
+          <h3>{userName}</h3>
         </div>
       </div>
     </div>
