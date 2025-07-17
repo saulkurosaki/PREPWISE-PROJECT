02480244246159a06df4d10f1feb04d5a822e0bc
saulkurosaki <saulkurosaki13@gmail.com>
2025-07-16 19:20:46 -0600

    feat(Agent.tsx): implement the call status handler buttons

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,37 +1,61 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 
+enum CallStatus {
+  INACTIVE = "INACTIVE",
+  CONNECTING = "CONNECTING",
+  ACTIVE = "ACTIVE",
+  FINISHED = "FINISHED",
+}
+
 const Agent = ({ userName, userId, type }: AgentProps) => {
+  const callStatus = CallStatus.FINISHED;
   const isSpeaking = true;
 
   return (
-    <div className="call-view">
-      <div className="card-interviewer">
-        <div className="avatar">
-          <Image
-            src="/ai-avatar.png"
-            alt="vapi"
-            width={65}
-            height={54}
-            className="object-cover"
-          />
-          {isSpeaking && <span className="animate-speak"></span>}
+    <>
+      <div className="call-view">
+        <div className="card-interviewer">
+          <div className="avatar">
+            <Image
+              src="/ai-avatar.png"
+              alt="vapi"
+              width={65}
+              height={54}
+              className="object-cover"
+            />
+            {isSpeaking && <span className="animate-speak"></span>}
+          </div>
+          <h3>AI Interviewer</h3>
         </div>
-        <h3>AI Interviewer</h3>
+
+        <div className="card-border">
+          <div className="card-content">
+            <Image
+              src="/ai-avatar.png"
+              alt="user avatar"
+              width={540}
+              height={540}
+              className="object-cover rounded-full size-[120px]"
+            />
+            <h3>{userName}</h3>
+          </div>
+        </div>
       </div>
 
-      <div className="card-border">
-        <div className="card-content">
-          <Image
-            src="/ai-avatar.png"
-            alt="user avatar"
-            width={540}
-            height={540}
-            className="object-cover rounded-full size-[120px]"
-          />
-          <h3>{userName}</h3>
-        </div>
+      <div className="w-full flex justify-center">
+        {callStatus !== "ACTIVE" ? (
+          <button className="relative btn-call">
+            <span className={cn("absolute animate-ping rounded-full opacity-75", callStatus !== "CONNECTING" && "hidden")} />
+            <span>
+              {callStatus === "INACTIVE" || "FINISHED" ? "Call" : ". . ."}
+            </span>
+          </button>
+        ) : (
+          <button className="btn-disconnect">End</button>
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
